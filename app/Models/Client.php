856a9449cd89b5f46da1deb274693ec3dbccbd8f
maganasamuel <maganasamuel001@gmail.com>
2021-03-24 20:14:41 +0800

    Set client relationships

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Payroll;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,13 +12,18 @@ class Client extends Model
 
     protected $guarded = [];
 
+    public function setAnnualPremiumAttribute($value)
+    {
+        $this->attributes['annual_premium'] = $value * 100;
+    }
+
     public function getAnnualPremiumCurrencyAttribute()
     {
         return number_format($this->annual_premium / 100, 2);
     }
 
-    public function setAnnualPremiumAttribute($value)
+    public function payrolls()
     {
-        $this->attributes['annual_premium'] = $value * 100;
+        return $this->belongsToMany(Payroll::class, 'payroll_clients');
     }
 }

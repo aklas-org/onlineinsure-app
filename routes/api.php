68b4e0cc11a5bd96c605f87fd9589c6d4e22d9f0
maganasamuel <maganasamuel001@gmail.com>
2021-03-25 21:53:22 +0800

    Removed api routes, controllers and json resources

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::middleware(['auth:sanctum'])->name('api.')->group(function () {
-    Route::get('sales-reps', [\App\Http\Controllers\Api\SalesRepController::class, 'index'])->name('sales-rep.index');
-});

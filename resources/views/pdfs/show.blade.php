66d5fc5031f1f0bfd16c18ce28bb483bb0d92964
maganasamuel <maganasamuel001@gmail.com>
2021-03-25 22:05:20 +0800

    Display payroll info

--- resources/views/pdfs/show.blade.php
+++ resources/views/pdfs/show.blade.php
@@ -4,6 +4,9 @@
     </x-slot>
 
     <x-page-content>
-
+        <div class="flex items-center justify-between">
+            <div>Adviser No: {{ $payroll->salesRep->id }}<span class="ml-3">{{ $payroll->salesRep->name }}</span></div>
+            <div></div>
+        </div>
     </x-page-content>
 </x-app-layout>

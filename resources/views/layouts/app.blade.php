30d18884520da766a44150106fef5ffbc6e69a63
maganasamuel <maganasamuel001@gmail.com>
2021-03-26 11:13:33 +0800

    Applied print media screen to blade layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,11 +24,11 @@
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100">
+    <div class="min-h-screen bg-gray-100 print:bg-transparent">
         @include('layouts.navigation')
 
         <!-- Page Heading -->
-        <header class="bg-white shadow">
+        <header class="bg-white shadow print:hidden">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 {{ $header }}
             </div>
